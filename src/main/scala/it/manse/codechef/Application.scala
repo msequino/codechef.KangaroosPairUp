74dab1b354fcb832fde137970da5fefe97947120
Manuel Sequino Ats <manuel.sequino@atscom.it>
2019-07-06 10:11:52 +0200

    committing kangaroo challenge

---
+++ src/main/scala/it/manse/codechef/Application.scala
@@ -0,0 +1,15 @@
+package it.manse.codechef
+
+object Application extends App {
+
+  override def main(args: Array[String]) : Unit = {
+    val M = 12
+    val pairs = for { a <- 1 to M; b <- 1 to a if stepSameFootStep(a, b, M) && arrivaAtEnd(a, b, M)} yield (a, b)
+
+    pairs.foreach { case (a, b) => println(s"($a, $b)") }
+  }
+  private def stepSameFootStep(a: Int, b: Int, M: Int) =  !(for( x <- 1 until M / a; y <- 1 until M / b ) yield {
+    if (a * x == b * y) true else false
+  }).exists( atLeastOneTrue => atLeastOneTrue)
+  private def arrivaAtEnd(a: Int, b: Int, M: Int) = M % b == 0 && M % a == 0
+}
