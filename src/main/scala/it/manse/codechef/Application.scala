35d7f27bd0095bbc2354dd73f9007cc39302719b
Manuel Sequino Ats <manuel.sequino@atscom.it>
2019-07-06 10:19:14 +0200

    refactor method

--- src/main/scala/it/manse/codechef/Application.scala
+++ src/main/scala/it/manse/codechef/Application.scala
@@ -4,12 +4,12 @@ object Application extends App {
 
   override def main(args: Array[String]) : Unit = {
     val M = 12
-    val pairs = for { a <- 1 to M; b <- 1 to a if stepSameFootStep(a, b, M) && arrivaAtEnd(a, b, M)} yield (a, b)
+    val pairs = for { a <- 1 to M; b <- 1 to a if stepSameFootStep(a, b, M) && arriveAtEnd(a, b, M)} yield (a, b)
 
     pairs.foreach { case (a, b) => println(s"($a, $b)") }
   }
   private def stepSameFootStep(a: Int, b: Int, M: Int) =  !(for( x <- 1 until M / a; y <- 1 until M / b ) yield {
     if (a * x == b * y) true else false
   }).exists( atLeastOneTrue => atLeastOneTrue)
-  private def arrivaAtEnd(a: Int, b: Int, M: Int) = M % b == 0 && M % a == 0
+  private def arriveAtEnd(a: Int, b: Int, M: Int) = M % b == 0 && M % a == 0
 }
